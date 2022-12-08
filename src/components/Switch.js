3e1eaddc1a0cbfdc9ae63d453679db12082be740
giannischiout <giannis.chiout@gmail.com>
2022-12-08 16:59:33 +0200

    react native icons

---
+++ src/components/Switch.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { StyleSheet, View, TouchableOpacity, Pressable } from "react-native";
+
+
+const Switch = () => {
+  return (
+    <Pressable>
+      <View></View>
+    </Pressable>
+  )
+}
+
+export default Switch
